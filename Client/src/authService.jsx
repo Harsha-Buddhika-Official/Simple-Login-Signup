b55d3456174865ead02fa060de38f7d0b75f27f7
Harsha Buddhika <59781888+Harsha-Buddhika-Official@users.noreply.github.com>
2025-06-29 00:17:04 +0530

    Update authService.jsx

--- Client/src/authService.jsx
+++ Client/src/authService.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const registerUser = async (userData) => {
-    return await axios.post('http://localhost:3001/register', userData);
+    return await axios.post('http://localhost:3000/register', userData);
 };
